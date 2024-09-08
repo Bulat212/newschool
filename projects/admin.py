46c0565c854ba28fd53ac0521a4105579c85889b
Bulat212 <bulatmukhutdinov22@gmail.com>
2024-09-08 22:28:43 +0300

    setting up the admin panel, add model Project, CpmpleteProject

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from projects.models import Project
 
-admin.site.register(Project)
+from projects.models import Project, СompleteProject
+
+# admin.site.register(Project)
+admin.site.register(СompleteProject)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields= {'slug':('name',)}
+
+
+# @admin.register(СompleteProject)
+# class СompleteProjectAdmin(admin.ModelAdmin):
+#     # name = Project.objects.get(pk = project.name)
+    # prepopulated_fields= {'slug':('name',)}
--- projects/models.py
+++ projects/models.py
@@ -1,18 +1,37 @@
-from tabnanny import verbose
+# from tabnanny import verbose
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
-class Project(models.Model):
-    name = models.CharField(max_length=200, unique=True, verbose_name='Название')
+class Project(models.Model): 
     slug = models.SlugField(max_length=200, unique=True, null=True, blank=True, verbose_name='Слаг')
+    name = models.CharField(max_length=200, unique=True, verbose_name='Название')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
-    link_to_git = models.URLField(max_length=200, unique=True, blank=True, null=True, verbose_name='Ссылка на гит')
-    hours_to_complete = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Часы на выполнение')
-    necessary_level = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Необходимый уровень')
-    experience = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Опыт за проект')
+    hoursToComplete = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Часы на выполнение')
+    necessaryLevel = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Необходимый уровень')
+    taskExperience = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Опыт за проект')
 
     class Meta:
         # db_table = 'project'
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.name
+
+
+class СompleteProject(models.Model):
+    student = models.IntegerField(verbose_name="Студент")
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, verbose_name="Проект") #если удалим проект, то удалятся и все завершенные проекты
+    leadTime = models.DurationField(blank=True, null=True, verbose_name="Время выполнения")
+    gainedExperience = models.IntegerField(verbose_name="Полученный опыт")
+    is_completed = models.BooleanField(default=False, verbose_name="Статус выполнения")
+
+
+    class Meta:
+        verbose_name = 'Завершенный проект'
+        verbose_name_plural = 'Завершенные проекты'
+
+    def __str__(self):
+        return "_".join([self.project.name, 'student', str(self.student)])
